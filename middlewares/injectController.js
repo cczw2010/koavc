19d4fc1686a86504642fe8af56b2028d82dc71d6
awen <71752352@qq.com>
2022-09-03 23:21:10 +0800

    v1.3.3, 增加全局路由中间件支持，增加自定义controller的扩展属性支持

---
+++ middlewares/injectController.js
@@ -0,0 +1,29 @@
+const extKeyName = "_extparams"
+
+/**
+ * 注入获取扩展参数方法(ctx.getControllerExtParam)只有进入router中有效,只最后一个匹配的router的参数，
+ **/
+export default (ctx,next)=>{
+  ctx.getControllerExtParam = function(paramName){
+    return getControllerExtParam(this,paramName)
+  }
+  return next()
+}
+
+/**
+ * 设置某个route实体的扩展参数，routerLayer为stack中的layer
+ */
+export function setControllerExtParams(routerLayer,extparams){
+  routerLayer[extKeyName] = extparams
+}
+/**
+ *  只有进入router中有效,只最后一个匹配的router，
+ */
+function getControllerExtParam(ctx,paramName){
+  if(ctx.router && ctx.matched && ctx.matched.length>1){
+    const routerLayer = ctx.matched[ctx.matched.length-1]
+    return routerLayer[extKeyName][paramName]
+  }
+  return null
+}
+
